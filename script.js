f7c217db55efd456b0e604460f456fe03f1e522a
Anthony Guarneri <guarneriAnthony@outlook.fr>
2023-03-01 11:17:21 +0100

    Mis en place de la doc

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-var addToDoButton = document.getElementById('addToDo');
-var toDoContainer = document.getElementById('toDoContainer');
-var inputField = document.getElementById('inputField');
+var addToDoButton = document.getElementById('addToDo');         // Je creer une variable lié au button, avec le content de "addToDo"
+var toDoContainer = document.getElementById('toDoContainer');   // Je creer une variable lié au container avec le content de "toDoContainer"
+var inputField = document.getElementById('inputField');         
 
-addToDoButton.onclick = function(){
-    if(inputField.value != ""){
-        var paragraph = document.createElement('p')
+addToDoButton.onclick = function(){                             // C'est une fonction "execution" qui se declanche quand on click 
+    if(inputField.value != ""){                                 // Je controle que quand on click inputField ne soit pas vide 
+        var paragraph = document.createElement('p')             // Si inputField n'ai pas vide, je creer une var avec comme value un 'p'
     }
-    paragraph.innerText = inputField.value;
+    paragraph.innerText = inputField.value;                     // Je valorise le paragraphe avec la valeur de inputField.value
 
-    paragraph.classList.add('paragraphe_style');
+    paragraph.classList.add('paragraphe_style');                //Je creer une classList "baliseCss" pour la styliser 
 
-    toDoContainer.appendChild(paragraph);
+    toDoContainer.appendChild(paragraph);                       // J'insere le paragraphe dans l'element toDoContainer
 
-    inputField.value = "";
+    inputField.value = "";                                      // Je vide l'input quand le paragraphe est ajoute 
 
-    paragraph.addEventListener('click',function(){
+    paragraph.addEventListener('click',function(){              //C'est une function qui, quand on click on ajoute un effect de style dans le css
         paragraph.classList.add('paragraph_click');
     })
 
-    paragraph.addEventListener('dblclick',function(){
+    paragraph.addEventListener('dblclick',function(){           // Function eventListener, quand on double  click, on remove l'enfant de toDoContainer
        toDoContainer.removeChild(paragraph); 
     })
 }
